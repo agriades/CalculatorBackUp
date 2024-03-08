43e2e9087e8b87099238c9617ec0e451229acf6c
Sooyoung Hwang <agriades23@gmail.com>
2024-03-08 16:29:03 +0900

    야호! 드디어 앱이 정상 작동한다!

--- app/src/main/java/com/example/calculatorbackup/Simple.kt
+++ app/src/main/java/com/example/calculatorbackup/Simple.kt
@@ -5,4 +5,5 @@ class Simple {
     fun calSubtract (currentTotal : String, valueToSubtract: String) = (currentTotal.toFloat() - valueToSubtract.toFloat()).toString()
     fun calMultiply (currentTotal: String, valueToMultiply: String) = (currentTotal.toFloat() * valueToMultiply.toFloat()).toString()
     fun calDivide (currentTotal: String, valueToDivide: String) = (currentTotal.toFloat() / valueToDivide.toFloat()).toString()
+    //진짜 브랜치가 파여졌나?
 }
